1228b6857657a9ddc6c9db6bcd18c20b3f116f36
TamUIC <tam.rice.cs@gmail.com>
2023-12-18 16:05:15 -0600

    A repo to work on basic tree funcs

---
+++ trees.cpp
@@ -0,0 +1,18 @@
+class node {
+    public:
+        int val = 0;
+        node* left = nullptr;
+        node* right = nullptr;
+
+        node(int value) {
+            val = value;
+        }
+
+        node(int value, node* l, node* r) {
+            val = value;
+            left = l;
+            right = r;
+        }
+};
+
+node* root = new node(0);
